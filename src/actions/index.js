dd573f9984763d59f07a0b8efa211ddb44664153
Jessica Poemape <jpoemape@dev2025.younique.local>
2018-08-31 15:12:02 -0600

    more changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid/v4');
 import { FETCH_CLASSROOMS_LIST, FETCH_DECKS_LIST, FETCH_STUDENTS_LIST, FETCH_STUDENT } from './types';
 import studentList from './../mocks/studentList';
 import classroomList from './../mocks/classroomList';
@@ -34,12 +35,56 @@ export const fetchDecksList = (classroomId) => async (dispatch) => {
     }
 };
 
-export const fetchStudent = (classroomId, studentId) => async (dispatch) => {
+export const fetchStudent = (classroomId, studentId) => (dispatch) => {
     if (!testing) {
-        try {
-        } catch (err) {
-        }
+        fetch('https://classroom-tracker.firebaseio.com/students.json')
     } else {
         dispatch({ type: FETCH_STUDENT, payload: studentList[0] });
+    }
+};
+
+export const addClassroom = (classroomInfo) => (dispatch) => {
+    if (!testing) {
+        const classroomData = {
+            id: uuid(),
+            teacher: classroomInfo.teacher,
+            label: classroomInfo.label,
+        }
+        fetch ('https://classroom-tracker.firebaseio.com/classrooms.json', {
+            method: 'POST',
+            body: JSON.stringify(classroomData),
+        })
+        .catch(err => console.log('Error adding a classroom: ', err))
+        .then(res => res.json) 
+        .then(parsedRes => {
+            console.log('RES:: ', parsedRes);
+        });
+    } else {
+        dispatch({ type: ADD_CLASSROOM, payload: {} });
+    }
+};
+
+export const addStudent = (studentInfo) => (dispatch) => {
+    if (!testing) {
+        const studentData = {
+            id: uuid(),
+            classroomId: studentInfo.classroomId,
+            firstName: studentInfo.firstName,
+            lastName: studentInfo.lastName,
+            label: studentInfo.label,
+            studiedDecks: [],
+        }
+        fetch ('https://classroom-tracker.firebaseio.com/students.json', {
+            method: 'POST',
+            body: JSON.stringify(studentData),
+        })
+        .catch(err => console.log('Error adding student: ', err))
+        .then(res => res.json)
+        .then(parsedRes => {
+            console.log('RES:: ', parsedRes);
+        });
+    
+    } else {
+        dispatch({ type: ADD_STUDENT, payload: {} });
     }
 };
